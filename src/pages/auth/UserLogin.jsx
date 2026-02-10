ff1b3c8d8df0c7304b0c4b0e64c116bf028604c1
Sheikh Uzair Raza <sheikhuzairraza834@gmail.com>
2026-02-11 00:57:50 +0500

    Remove some logs

--- src/pages/auth/UserLogin.jsx
+++ src/pages/auth/UserLogin.jsx
@@ -12,7 +12,6 @@ const UserLogin = () => {
 
     const email = e.target.email.value;
     const password = e.target.password.value;
-    console.log(import.meta.env.VITE_BACKEND_BASE_URL);
     const response = await axios.post(`${import.meta.env.VITE_BACKEND_BASE_URL}/api/auth/user/login`, {
       email,
       password
--- src/pages/auth/UserRegister.jsx
+++ src/pages/auth/UserRegister.jsx
@@ -12,7 +12,6 @@ const UserRegister = () => {
         e.preventDefault();
 
         const firstName = e.target.firstName.value;
-        console.log(firstName);
         const lastName = e.target.lastName.value;
         const email = e.target.email.value;
         const password = e.target.password.value;
